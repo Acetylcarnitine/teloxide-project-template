a9020c3e3bb01e8b5d4fa4a44b7ec50b321ba2f5
sigma1337 <darbuz449@gmail.com>
2024-06-01 14:26:38 +0300

    teloxide project template 1.0

--- src/database/traits/reader.rs
+++ src/database/traits/reader.rs
@@ -2,15 +2,6 @@ use std::sync::Arc;
 use sqlx::{Pool, Postgres};
 
 pub trait DefaultReader {
-    async fn read_one(pool: Arc<Pool<Postgres>>, id: i64) -> Result<Self, sqlx::Error>
-        where Self: Sized
-    {
-        panic!("Trait does not implemented for this struct");
-    }
-
-    async fn read_all(pool: Arc<Pool<Postgres>>) -> Result<Vec<Self>, sqlx::Error>
-        where Self: Sized
-    {
-        panic!("Trait does not implemented for this struct");
-    }
+    async fn read_one(pool: Arc<Pool<Postgres>>, id: i64) -> Result<Self, sqlx::Error> where Self: Sized;
+    async fn read_all(pool: Arc<Pool<Postgres>>) -> Result<Vec<Self>, sqlx::Error> where Self: Sized;
 }
